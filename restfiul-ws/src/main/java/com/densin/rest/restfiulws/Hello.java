f4cc3fdf708af6c99b147d5a82deaa6285e5f592
Basharabulaban <ba.ahmad@gmail.com>
2022-02-19 01:05:26 +0300

    update the class

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/Hello.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/Hello.java
@@ -1,13 +1,23 @@
 package com.densin.rest.restfiulws;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class Hello {
+	
 	@RequestMapping(method=RequestMethod.GET, path="hi")
+	
 	public String hi() {
 		return "HI";
 		
 	}
+	
+	@GetMapping( path="hello")
+	public String his() {
+		return "hello";
+		
+	}
+
 }
