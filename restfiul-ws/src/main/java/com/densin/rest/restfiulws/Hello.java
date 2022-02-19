5596aa4de32646819b3ec4fd7a7d324bc9cf86df
Basharabulaban <ba.ahmad@gmail.com>
2022-02-19 17:08:29 +0300

    commit any chnages

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/Hello.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/Hello.java
@@ -1,11 +1,14 @@
 package com.densin.rest.restfiulws;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")     // ythis to advise spring to allow recieve request from http://localhost:4200
+
 public class Hello {
 	
 	@RequestMapping(method=RequestMethod.GET, path="hi")
@@ -22,7 +25,7 @@ public class Hello {
 	}
 	@GetMapping( path="hello-world-been")
 	public HellowWorldBean hellowWorldBean() {
-		return  new HellowWorldBean("Hello World");
+		return  new HellowWorldBean("Hello World bean");
 		
 	}
 	
