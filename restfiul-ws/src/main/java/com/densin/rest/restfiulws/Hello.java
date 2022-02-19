212b1f52feaf6f8baf027ea4f7b549a6ef9069b8
Basharabulaban <ba.ahmad@gmail.com>
2022-02-19 04:36:06 +0300

    commit helloWorldBean

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/Hello.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/Hello.java
@@ -1,6 +1,7 @@
 package com.densin.rest.restfiulws;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,6 +23,12 @@ public class Hello {
 	@GetMapping( path="hello-world-been")
 	public HellowWorldBean hellowWorldBean() {
 		return  new HellowWorldBean("Hello World");
+		
+	}
+	
+	@GetMapping( path="hello-world-path-variable/{name}")
+	public HellowWorldBean hellowWorldBean(@PathVariable String name) {
+		return  new HellowWorldBean(String.format("Hello World,%s",name));
 		
 	}
 
