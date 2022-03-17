c322799218b2ef66f8b2d6fc88a8a207e70ee6ed
Basharabulaban <ba.ahmad@gmail.com>
2022-03-18 01:51:00 +0300

    solve requestmaphandlemap issue

--- restfiul-ws/src/main/resources/data.sql
+++ restfiul-ws/src/main/resources/data.sql
@@ -1,1 +1,8 @@
 insert into Todo(id, username,description,target_Date,is_Done) values(10001,'Basharusr','learn JPA',sysdate(),false);
+insert into Todo(id, username,description,target_Date,is_Done) values(10002,'Basharusr','learn React',sysdate(),false);
+insert into Todo(id, username,description,target_Date,is_Done) values(10003,'Basharusr','learn Spring Boot',sysdate(),false);
+insert into Todo(id, username,description,target_Date,is_Done) values(10004,'Basharusr','learn Microservices',sysdate(),false);
+insert into Todo(id, username,description,target_Date,is_Done) values(10005,'Basharusr','learn HTML',sysdate(),false);
+insert into Todo(id, username,description,target_Date,is_Done) values(10006,'Basharusr','learn Javascript',sysdate(),false);
+insert into Todo(id, username,description,target_Date,is_Done) values(10007,'Basharusr','learn CSS3',sysdate(),false);
+
