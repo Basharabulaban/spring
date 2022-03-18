e21d901f148677b44b698292173e64cf93d1cf02
Basharabulaban <ba.ahmad@gmail.com>
2022-03-18 19:53:15 +0300

    book table

---
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/Todo/Book.java
@@ -0,0 +1,32 @@
+package com.densin.rest.restfiulws.Todo;
+
+public class Book {
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", auther=" + auther + "]";
+	}
+
+	long id;
+	String name;
+	String auther;
+
+	public Book(long id, String name, String auther) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.auther = auther;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAuther() {
+		return auther;
+	}
+
+}
