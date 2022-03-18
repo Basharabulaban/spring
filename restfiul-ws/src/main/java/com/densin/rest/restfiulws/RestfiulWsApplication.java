fd7e3a40a05e8cbf59e1bbf8be6ca85df1b254cb
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 00:02:43 +0300

    ok

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/RestfiulWsApplication.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/RestfiulWsApplication.java
@@ -3,14 +3,21 @@ package com.densin.rest.restfiulws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-
+//////  spring auto configuration is so important once you go deeply in spring boot
 public class RestfiulWsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestfiulWsApplication.class, args);
+//	ApplicationContext applicationContext=	
+			SpringApplication.run(RestfiulWsApplication.class, args);
+//	for (String name : applicationContext.getBeanDefinitionNames()) {
+//		System.out.println(name);
+//		
+//	}
+	
 	}
 
 }
