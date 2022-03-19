f9835da181309490e8332f860a208a132aca64e5
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 22:35:13 +0300

    add notes to Source code

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserRepository.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserRepository.java
@@ -6,3 +6,10 @@ import com.densin.rest.restfiulws.User.User;
 public interface UserRepository extends JpaRepository<User,Long>  {  // entity you need to manage User
 
 }
+
+
+//use simple user repository instead of DAO services , instead of writing a lot of Dao Services , 
+// with simmillar logic can use spring data jpa such as  JpaRepository
+
+// JpaRepository   is common abstraction to be extended , which will get all methods to such as save , check this and understand it as it is used to manage en
+
