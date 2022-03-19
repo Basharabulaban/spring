873245a2e4bf2e5fcdacf665bef7223f3d5a723d
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 21:16:52 +0300

    add user in command line runner

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserDawServiceCommandLineRunner.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserDawServiceCommandLineRunner.java
@@ -2,8 +2,11 @@ package com.densin.rest.restfiulws;
 
 import javax.persistence.PersistenceContext;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.densin.rest.restfiulws.User.User;
 @Component  // to be managed from spring services
 
 public class UserDawServiceCommandLineRunner implements CommandLineRunner{
@@ -11,7 +14,7 @@ public class UserDawServiceCommandLineRunner implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
+		User user = new User("Bashar","Admin");
 	}
 
 }
