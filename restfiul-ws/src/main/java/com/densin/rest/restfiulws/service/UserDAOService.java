019cdfd3b7124a0214a33f0456237da8b1b16c5e
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 21:06:09 +0300

    @PersistenceContext

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserDAOService.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserDAOService.java
@@ -1,6 +1,7 @@
 package com.densin.rest.restfiulws.service;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,13 @@ import com.densin.rest.restfiulws.User.User;
 
 @Repository
 @Transactional
+
+// persistent
+//User jill = new User("Jack",
+
 public class UserDAOService {
+	@PersistenceContext
+	
 private EntityManager entityManager;
 public long insert (User user) {
 	// open Transacton 
