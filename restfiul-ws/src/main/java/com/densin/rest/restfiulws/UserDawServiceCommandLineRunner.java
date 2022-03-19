0be49eac18ee9967612db4cdc553d0f5af9bf0de
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 21:20:28 +0300

    @Autowired private UserDAOService userDaoService;

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserDawServiceCommandLineRunner.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserDawServiceCommandLineRunner.java
@@ -2,19 +2,26 @@ package com.densin.rest.restfiulws;
 
 import javax.persistence.PersistenceContext;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.densin.rest.restfiulws.User.User;
+import com.densin.rest.restfiulws.service.UserDAOService;
 @Component  // to be managed from spring services
 
 public class UserDawServiceCommandLineRunner implements CommandLineRunner{
 
+	
+	@Autowired private UserDAOService userDaoService;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		User user = new User("Bashar","Admin");
+	long insert=	userDaoService.insert(user);
+		
+		
 	}
 
 }
