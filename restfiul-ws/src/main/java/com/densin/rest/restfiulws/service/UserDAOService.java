ec0c7e0ff3341345419c2d1a7e9d5d1ead3f5049
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 20:59:29 +0300

    return the id created

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserDAOService.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserDAOService.java
@@ -2,7 +2,14 @@ package com.densin.rest.restfiulws.service;
 
 import javax.persistence.EntityManager;
 
+import com.densin.rest.restfiulws.User.User;
+
 public class UserDAOService {
 private EntityManager entityManager;
+public long insert (User user) {
+	
+	entityManager.persist(user);
+	return user.getId();
+}
 
 }
