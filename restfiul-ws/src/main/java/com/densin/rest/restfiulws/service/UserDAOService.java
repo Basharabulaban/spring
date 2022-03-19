c94a9baf37cf53a97eb256110e7b1ab43eb74f8c
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 21:02:32 +0300

    add Repository

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserDAOService.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/service/UserDAOService.java
@@ -2,13 +2,19 @@ package com.densin.rest.restfiulws.service;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.stereotype.Repository;
+
 import com.densin.rest.restfiulws.User.User;
+
+
+@Repository
 
 public class UserDAOService {
 private EntityManager entityManager;
 public long insert (User user) {
-	
-	entityManager.persist(user);
+	// open Transacton 
+	entityManager.persist(user);  // make change
+	// close transaction
 	return user.getId();
 }
 
