f5e16585fd0d4fc00bac6a0472651870c7975fbe
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 22:48:59 +0300

    use some new service in JPA repository such as findall findbyid

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserRepositoryServiceCommandLineRunner.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserRepositoryServiceCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.densin.rest.restfiulws;
 
-import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,22 +10,34 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.densin.rest.restfiulws.User.User;
-import com.densin.rest.restfiulws.service.UserDAOService;
-import com.densin.rest.restfiulws.service.UserRepository;
-@Component  // to be managed from spring services
 
-public class UserRepositoryServiceCommandLineRunner implements CommandLineRunner{
-private static final Logger log= LoggerFactory.getLogger(UserRepositoryServiceCommandLineRunner.class);
-	
-	@Autowired private UserRepository userRepository;
-	
+import com.densin.rest.restfiulws.service.UserRepository;
+
+@Component // to be managed from spring services
+
+public class UserRepositoryServiceCommandLineRunner implements CommandLineRunner {
+	private static final Logger log = LoggerFactory.getLogger(UserRepositoryServiceCommandLineRunner.class);
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		User user = new User("KOKO","Admin");
+		User user = new User("KOKO", "Admin");
+		userRepository.save(user);
 //	User insert=userRepository.save(user);
-	userRepository.save(user);	
-		log.info("New User is created:"+user);
+
+		log.info("New User is retrieved:" + user);
+
+		Optional<User> specficUser = userRepository.findById(2L);
+
+		log.info("find user 1" + specficUser);
+
+		log.info("find user2" + userRepository.findById(1L));
+
+		List<User> users = userRepository.findAll();
+
 	}
 
 }
