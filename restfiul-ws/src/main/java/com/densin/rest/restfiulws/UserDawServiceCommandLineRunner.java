33bf79769c599b1577193efcdef0d83495ffc96d
Basharabulaban <ba.ahmad@gmail.com>
2022-03-19 21:14:47 +0300

    add unimplmneted command line runner

--- restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserDawServiceCommandLineRunner.java
+++ restfiul-ws/src/main/java/com/densin/rest/restfiulws/UserDawServiceCommandLineRunner.java
@@ -3,7 +3,15 @@ package com.densin.rest.restfiulws;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component  // to be managed from spring services
 
 public class UserDawServiceCommandLineRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
